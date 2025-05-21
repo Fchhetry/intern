9809d85c3dcdee303af40ac4cfcb096dfbf816cc
Fchhetry <poojafuyal20@gmail.com>
2025-05-21 16:13:45 +0545

    LetVarConst

---
+++ LetVarConst.js
@@ -0,0 +1,11 @@
+console.log("difference between let, var and const")
+var a = 45
+let b = "POOJA"
+var c = null
+var d = undefined
+const author = "POOJA"
+{
+    let b = "this"
+    console.log(b)
+}
+console.log(b)
