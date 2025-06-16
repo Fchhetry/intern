7a11f510747f21972ac7865d460846ed474e40b8
Fchhetry <poojafuyal20@gmail.com>
2025-06-16 17:07:55 +0545

    'comment'

--- student/src/store/store.js
+++ student/src/store/store.js
@@ -12,17 +12,19 @@ const persistConfig = {
 };
 
 const rootReducer = combineReducers({
-  students: studentReducer,
+  students: studentReducer, // cobines all reucers to a single root reducer here students
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer); // wraps root reducer with persist capabilities
+//handles saving the state to localStorage and rehydrating it 
 
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false, // important for redux-persist to work
-    }).concat( logger),
+      // redux persist stores non serializable metadata which causes warnings/errors .
+    }).concat( logger), // adds redux logger middleware for debugging- logs every action and state change in the console
 });
 
 export const persistor = persistStore(store);
